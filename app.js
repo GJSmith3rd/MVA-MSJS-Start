c377b4da2b54e5f42ca86631b2c01bcd17aa0259
Gilbert Smith <gjsmith3rd@gmail.com>
2015-07-09 11:43:47 -0500

    Changed session password.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'password'
+  secret: 'super-duper-secret'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
